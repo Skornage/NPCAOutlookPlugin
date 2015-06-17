c40f33d8eff589573fccbc4f9b7dbc4bbaa0f1fd
Skornage <millerna@rose-hulman.edu>
2015-06-17 15:54:37 -0400

    updated DisplayAccountInformation method

--- OutlookAddInTest/FormTest.cs
+++ OutlookAddInTest/FormTest.cs
@@ -46,11 +46,6 @@ namespace OutlookAddInTest
             }         
         }
 
-		private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
-		{
-
-		}
-
 		private void Cancel_Click(object sender, EventArgs e)
 		{
 			this.Close();
@@ -128,11 +123,6 @@ namespace OutlookAddInTest
             dataGridView1.DataSource = bs;
         }
 
-        private void FormTest_Load(object sender, EventArgs e)
-        {
-
-        }
-
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             dataGridView1.CurrentRow.Selected = true;
@@ -155,53 +145,15 @@ namespace OutlookAddInTest
 
         public static void DisplayAccountInformation(Outlook.Application application)
         {
-
-            // The Namespace Object (Session) has a collection of accounts.
             Outlook.Accounts accounts = application.Session.Accounts;
-
-            // Concatenate a message with information about all accounts.
             StringBuilder builder = new StringBuilder();
 
-            // Loop over all accounts and print detail account information.
-            // All properties of the Account object are read-only.
             foreach (Outlook.Account account in accounts)
             {
-
-                // The DisplayName property represents the friendly name of the account.
                 builder.AppendFormat("DisplayName: {0}\n", account.DisplayName);
-
-                // The UserName property provides an account-based context to determine identity.
                 builder.AppendFormat("UserName: {0}\n", account.UserName);
-
-                // The SmtpAddress property provides the SMTP address for the account.
                 builder.AppendFormat("SmtpAddress: {0}\n", account.SmtpAddress);
-
-                // The AccountType property indicates the type of the account.
                 builder.Append("AccountType: ");
-                switch (account.AccountType)
-                {
-
-                    case Outlook.OlAccountType.olExchange:
-                        builder.AppendLine("Exchange");
-                        break;
-
-                    case Outlook.OlAccountType.olHttp:
-                        builder.AppendLine("Http");
-                        break;
-
-                    case Outlook.OlAccountType.olImap:
-                        builder.AppendLine("Imap");
-                        break;
-
-                    case Outlook.OlAccountType.olOtherAccount:
-                        builder.AppendLine("Other");
-                        break;
-
-                    case Outlook.OlAccountType.olPop3:
-                        builder.AppendLine("Pop3");
-                        break;
-                }
-
                 builder.AppendLine();
             }
 
