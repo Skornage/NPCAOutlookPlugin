106bcb813c5ec3492d61b3a8dbc63ed0fabc5430
greenjm <greenjm@rose-hulman.edu>
2015-06-17 15:45:08 -0400

    Added message box to display current user

--- OutlookAddInTest/FormTest.cs
+++ OutlookAddInTest/FormTest.cs
@@ -19,6 +19,7 @@ namespace OutlookAddInTest
 		public FormTest(Outlook.MailItem mailItem)
 		{
 			InitializeComponent();
+            DisplayAccountInformation(Globals.ThisAddIn.Application);
             populateDataGrid(0);
 			comboBox1.SelectedIndex = 0;
             textBox1.TextChanged += new EventHandler(searchTextChanged);
@@ -115,5 +116,63 @@ namespace OutlookAddInTest
             bs.Filter = string.Format("id LIKE '%{0}%' AND type LIKE '%{1}%' AND name LIKE '%{2}%' AND email LIKE '%{3}%' AND info LIKE '%{4}%'",
                                        id, type, name, email, info);
         }
-	}
+	
+
+
+        public static void DisplayAccountInformation(Outlook.Application application)
+        {
+
+            // The Namespace Object (Session) has a collection of accounts.
+            Outlook.Accounts accounts = application.Session.Accounts;
+
+            // Concatenate a message with information about all accounts.
+            StringBuilder builder = new StringBuilder();
+
+            // Loop over all accounts and print detail account information.
+            // All properties of the Account object are read-only.
+            foreach (Outlook.Account account in accounts)
+            {
+
+                // The DisplayName property represents the friendly name of the account.
+                builder.AppendFormat("DisplayName: {0}\n", account.DisplayName);
+
+                // The UserName property provides an account-based context to determine identity.
+                builder.AppendFormat("UserName: {0}\n", account.UserName);
+
+                // The SmtpAddress property provides the SMTP address for the account.
+                builder.AppendFormat("SmtpAddress: {0}\n", account.SmtpAddress);
+
+                // The AccountType property indicates the type of the account.
+                builder.Append("AccountType: ");
+                switch (account.AccountType)
+                {
+
+                    case Outlook.OlAccountType.olExchange:
+                        builder.AppendLine("Exchange");
+                        break;
+
+                    case Outlook.OlAccountType.olHttp:
+                        builder.AppendLine("Http");
+                        break;
+
+                    case Outlook.OlAccountType.olImap:
+                        builder.AppendLine("Imap");
+                        break;
+
+                    case Outlook.OlAccountType.olOtherAccount:
+                        builder.AppendLine("Other");
+                        break;
+
+                    case Outlook.OlAccountType.olPop3:
+                        builder.AppendLine("Pop3");
+                        break;
+                }
+
+                builder.AppendLine();
+            }
+
+            // Display the account information.
+            System.Windows.Forms.MessageBox.Show(builder.ToString());
+        }
+    }
 }
