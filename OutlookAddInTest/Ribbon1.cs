b80237c0440044c847f18dd93ee7fca570f01d08
Skornage <millerna@rose-hulman.edu>
2015-06-19 14:40:30 -0400

    Fixed attach button

--- OutlookAddInTest/Ribbon1.cs
+++ OutlookAddInTest/Ribbon1.cs
@@ -8,25 +8,6 @@ using System.Text;
 using Outlook = Microsoft.Office.Interop.Outlook;
 using Office = Microsoft.Office.Core;
 
-// TODO:  Follow these steps to enable the Ribbon (XML) item:
-
-// 1: Copy the following code block into the ThisAddin, ThisWorkbook, or ThisDocument class.
-
-//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
-//  {
-//      return new Ribbon1();
-//  }
-
-// 2. Create callback methods in the "Ribbon Callbacks" region of this class to handle user
-//    actions, such as clicking a button. Note: if you have exported this Ribbon from the Ribbon designer,
-//    move your code from the event handlers to the callback methods and modify the code to work with the
-//    Ribbon extensibility (RibbonX) programming model.
-
-// 3. Assign attributes to the control tags in the Ribbon XML file to identify the appropriate callback methods in your code.  
-
-// For more information, see the Ribbon XML documentation in the Visual Studio Tools for Office Help.
-
-
 namespace OutlookAddInTest
 {
 	[ComVisible(true)]
@@ -50,7 +31,6 @@ namespace OutlookAddInTest
 		#endregion
 
 		#region Ribbon Callbacks
-		//Create callback methods here. For more information about adding callback methods, visit http://go.microsoft.com/fwlink/?LinkID=271226
 
 		public void Ribbon_Load(Office.IRibbonUI ribbonUI)
 		{
@@ -71,23 +51,21 @@ namespace OutlookAddInTest
 
 		private Outlook.MailItem getMailItem()
 		{
-			//try
-			//{
-			//	Object explorer = Globals.ThisAddIn.Application.ActiveWindow().Selection[1];
-			//	if (explorer is Outlook.MailItem)
-			//	{
-			//		return (Outlook.MailItem) explorer;
-			//	}
-			//	System.Diagnostics.Debug.WriteLine("The item you selected was not an email item.");
-			//	return null;
-			//}
-			//catch (System.Runtime.InteropServices.COMException)
-			//{
-			//	System.Windows.Forms.MessageBox.Show("You must select an Email first.");
-			//	return null;
-			//}
-			return null;
-
+			try
+			{
+				Object explorer = Globals.ThisAddIn.Application.ActiveWindow().Selection[1];
+				if (explorer is Outlook.MailItem)
+				{
+					return (Outlook.MailItem)explorer;
+				}
+				System.Diagnostics.Debug.WriteLine("The item you selected was not an email item.");
+				return null;
+			}
+			catch (System.Runtime.InteropServices.COMException)
+			{
+				System.Windows.Forms.MessageBox.Show("You must select an Email first.");
+				return null;
+			}
 		}
 
 		public void OnRemoveButton(Office.IRibbonControl control)
